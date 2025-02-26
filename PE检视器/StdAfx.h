6d1fe894332c530dc590f733c913a0534a44b60f
NiYeh <tsofu.yeh@gmail.com>
2025-02-26 14:53:53 +0800

    Update 1.0

--- PE检视器/StdAfx.h
+++ PE检视器/StdAfx.h
@@ -56,4 +56,5 @@ DWORD RvaToFileOffset(IN LPVOID pFileBuffer,IN DWORD dwRva);
 //{{AFX_INSERT_LOCATION}}
 // Microsoft Visual C++ will insert additional declarations immediately before the previous line.
 
+
 #endif // !defined(AFX_STDAFX_H__A9DB83DB_A9FD_11D0_BFD1_444553540000__INCLUDED_)
---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+# PE检视器🔍
+
+**使用VC6.0开发类似于PE LordPE软件的Win32程序**
+
+灵感来自[LordPE](https://baike.baidu.com/item/LordPE/4374254)
+
+<video src="D:\GitHub\PE-Viewer\Assets\video.mp4"></video>
+
+![img1](D:\GitHub\PE-Viewer\Assets\img1.png)
+
+![img2](D:\GitHub\PE-Viewer\Assets\img2.png)
